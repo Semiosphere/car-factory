fa7415d00d6d96d33f85d80e6585d262d77e5fb0
Zach Dugger <ztd522@gmail.com>
2022-01-13 14:40:18 -0500

    Practicing pushing current assignment to github

---
+++ main.js
@@ -0,0 +1,21 @@
+const createChassis = () => {
+    
+    const chassisObj = {}
+    
+    return chassisObj
+}
+
+const addWheels = (chassisObj) => {
+    
+    chassisObj.wheels = 4
+
+    return chassisObj
+}
+
+const addEngine = (chassisObj) => {
+    
+    chassisObj.engine = "4.8L"
+
+    return chassisObj
+}
+
